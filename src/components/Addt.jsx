275078b8795708168822d69c6ef438b3fe10d671
Ak <ankitk1lburgi@gmail.com>
2024-03-22 19:30:03 +0530

    update completed checkbox

--- src/components/Addt.jsx
+++ src/components/Addt.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React,{useState} from "react";
 function Addt(props){
+    const [is,setis]=useState(false);
     console.log(props);
     return <div  className="card">
         <h1>{props.title}</h1>
         <h3>{props.des}</h3>
         <p>{props.deadline}</p>
         <p>{props.priority}</p>
-        <button onClick={()=>props.ond(props.id)}> Delete</button>
+        <p><input type="checkbox" onChange={()=>setis(!is)}/><label htmlFor="">Completed</label></p>
+        {is&&(<button onClick={()=>props.ond(props.id)}> Delete</button>)}
         
     </div>
 }
